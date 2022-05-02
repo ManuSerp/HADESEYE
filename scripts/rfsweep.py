8d0aa669aac1ba223fa1514ff73d8d320d54fee5
Serp <manu.serpette74@gmail.com>
2022-05-02 15:24:50 +0200

    coding...

---
+++ scripts/rfsweep.py
@@ -0,0 +1,31 @@
+import subprocess
+import vargraph
+
+
+def bash_com(cmd="hackrf_sweep -f 2400:2490"):  # -r sample
+    bashCmd = cmd.split(" ")
+    process = subprocess.Popen(bashCmd, stdout=subprocess.PIPE)
+
+    output, error = process.communicate()
+
+    if not error:
+        return output
+    else:
+        return error
+
+
+def file_to_list(file_name):
+    with open(file_name, 'r') as f:
+        lines = f.readlines()
+    for i in range(len(lines)):
+        lines[i] = lines[i].strip('\n')
+        lines[i] = lines[i].split(', ')
+        for j in range(2, len(lines[i])):
+            lines[i][j] = float(lines[i][j])
+        var = lines[i][0:6]
+        var.append(lines[i][len(lines[i])-5: len(lines[i])])
+        lines[i] = var
+    return lines
+
+
+a = file_to_list("sample")
