cb1caba34f40a043df8526f15e24454a5c9c9d66
Serp <manu.serpette74@gmail.com>
2022-05-11 17:14:51 +0200

    install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,1 +1,13 @@
 pip install pyqtgraph
+
+
+sudo apt-get install build-essential cmake libusb-1.0-0-dev pkg-config libfftw3-dev
+wget https://github.com/greatscottgadgets/hackrf/releases/download/v2021.03.1/hackrf-2021.03.1.tar.xz
+tar -xvf hackrf-2021.03.1.tar.xz
+cd hackrf-2021.03.1
+mkdir host/build
+cd host/build
+cmake ..
+make
+sudo make install
+sudo ldconfig
