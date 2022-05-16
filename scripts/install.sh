18659f0fd67f5c450290d9e17878d4b72707a947
Nathan <nathan.galmiche@sfr.fr>
2022-05-16 09:29:24 +0200

    correc_install

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,9 @@
+python -m ensurepip --upgrade
 pip install pyqtgraph
 
 
 sudo apt-get install build-essential cmake libusb-1.0-0-dev pkg-config libfftw3-dev
+pip install sympy
 wget https://github.com/greatscottgadgets/hackrf/releases/download/v2021.03.1/hackrf-2021.03.1.tar.xz
 tar -xvf hackrf-2021.03.1.tar.xz
 cd hackrf-2021.03.1
